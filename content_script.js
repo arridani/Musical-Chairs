0a80dff28ee71b5e3443316ac2bd4f7f7e73bd32
arridani <arriana.daniels@yahoo.com>
2018-07-16 13:35:36 -0700

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var counter = 0;
-
+    
     $("#page").prepend(
       "<button onclick='window.Grooveshark.togglePlayPause()' id='pl'>Play/Pause " + counter + "</button>" +
       "<button onclick='window.Grooveshark.togglePlayPause()' id='timer'>Try it" + "</button>");
